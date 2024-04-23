7a6abbed927520d8cf5647b43686a72fae2ba64a
born-A <93516595+born-A@users.noreply.github.com>
2024-04-23 17:28:21 +0900

    [style]가이드 맞춰서 수정

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
-
+		
+		/* 코드 작성 */
 		System.out.print("수를 입력하시오 : ");
 		
 		int num = scanner.nextInt();
--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -6,6 +6,8 @@ public class Prob3 {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		
+		/* 코드 작성 */
 		int cnt = 1;
 		
 		while(cnt < 4) {
