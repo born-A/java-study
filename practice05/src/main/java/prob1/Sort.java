d77e057e668859454446dc771de0e3566efad469
born-A <93516595+born-A@users.noreply.github.com>
2024-04-26 09:36:34 +0900

    패키지 분리

--- practice05/src/main/java/prob1/Sort.java
+++ practice05/src/main/java/prob1/Sort.java
@@ -1,5 +1,7 @@
 package prob1;
 
+import java.util.Arrays;
+
 public class Sort {
 	
 	public static void main(String[] arg) {
@@ -16,6 +18,16 @@ public class Sort {
 		//
 		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
 		//
+		
+		for(int i=0; i<array.length-1; i++) {
+            for(int j=0; j<array.length-1-i; j++){
+                if(array[j] < array[j+1]) {
+                    int tmp = array[j];    
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }    
+            }
+        }    
 
 		
 		// 결과 출력
