216bfcd4a816e817f84e9351e08123d81c284029
born-A <93516595+born-A@users.noreply.github.com>
2024-04-26 15:34:00 +0900

    string 불변 타입, StringBuffer 실습

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,20 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		//c:\tmep 
+		System.out.println("c:\temp");
+		//한번 더 적으면 escape
+		System.out.println("c:\\temp");
+		
+		// "Hello" , \ 는 escape
+		System.out.println("\"hello\"");
+		
+		// \t : tab
+		// \r : carriage return
+		// \n : newline
+		// \b : beep
+	}
+
+}
